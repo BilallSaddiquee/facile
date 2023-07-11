df73a2b221f54fa122a33ce44f66bc544c3892be
Asfar Waheed <114741996+AsfarWaheed@users.noreply.github.com>
2023-07-11 13:38:55 +0500

    navigate
    navigate

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,4 +1,5 @@
 import react from 'react';
+import { Link } from "react-router-dom";
 import img1 from '../images/Facebook.png';
 import img3 from '../images/instagram.png';
 import img4 from '../images/Linkdin.png';
@@ -87,7 +88,7 @@ body {
     border: none;
     border-radius: 4px;
     cursor: pointer;
-    margin: 15px;
+    margin: 10px;
   }
   
   button:hover {
@@ -97,13 +98,13 @@ body {
   
   .using1 {
     text-align: center;
-    margin-top: 10px;
+    margin-top: 0;
   }
   
   .socials1 {
     display: flex;
     justify-content: center;
-    margin-top: 10px;
+    margin-top: 5px;
   }
   
   .socials1 img {
@@ -129,7 +130,7 @@ body {
                     <input type="password" placeholder="Password" required />
                     <a href="">forgot password?</a>
                     <button>Sign In</button>
-
+                    <p>Don't have a account <Link to="/">SIGN In</Link></p>
                 </div>
                 <div className="using1">
                     <p>Log in using</p>
