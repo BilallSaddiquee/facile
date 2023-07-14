baaaec9b0a87a39d88c704ddb03954ef5dfd515a
BilallSaddiquee <bilallsaddiquee@gmail.com>
2023-07-14 11:26:25 +0500

    popup pages
    create channel and Css of invitation

--- client/src/components/AddCoworker.js
+++ client/src/components/AddCoworker.js
@@ -1,5 +1,6 @@
 import react, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import img1 from "../images/logofecile.png";
 
 import axios from "axios";
 function AddCoworker({ onClose }){
@@ -45,28 +46,78 @@ const EmailHandler = (e) => {
  
   return (
     <>
+      <style>
+        {
+          `
+
+          img {
+            margin: 10px;
+          }
+
+          h2 {
+            paddingTop: 10px;
+            font-size: 3rem;
+            line-height: 1;
+            display: flex;
+            marginLeft: 10px;
+            }
+
+          span{
+            margin: 10px;
+          }
 
 
+          header-coworker button{
+            grid-column: 1 / span 2;
+            justify-self: center;
+            margin-top: 1rem;
+          }
+          button {
+            font-size: 1.7rem;
+            padding: 8px;
+            margin: 3px;
+            border: 1px solid #ddd;
+            border-radius: 0.5rem;
+            }
+          
+          input {
+              font-size: 1.7rem;
+              padding: 10px;
+              width: 100%;
+              margin: 4px;
+              border: 1px solid #ddd;
+              border-radius: 0.5rem;
+            }
+          `
+        }
+      </style>
 
-      <div className="container-fluid">
-        
-      <input
-            type="email"
-            placeholder="Email"
-            autoComplete="on"
-            onChange={EmailHandler}
-            value={email}
-          />
-          {err ? (
-            <span style={{ color: "rgb(247, 14, 14)" }}>
-              {errors}
-            </span>
-          ) : (
-            ""
-          )}
-          <button onClick={Addworker}>Send Invite</button>
-          <button onClick={onClose}>Close</button>
-         
+
+      <div className="header-coworker">
+        <img src={img1} alt="Facile" className="nav__logo" id="logo" />
+          <h2>
+              Invite your co workers
+            </h2>
+            <h3>
+              To:
+            </h3>
+        <input
+              type="email"
+              placeholder="name@email.com"
+              autoComplete="on"
+              onChange={EmailHandler}
+              value={email}
+            />
+            {err ? (
+              <span style={{ color: "rgb(247, 14, 14)" }}>
+                {errors}
+              </span>
+            ) : (
+              ""
+            )}
+            <button onClick={Addworker}>Send Invite</button>
+            <button onClick={onClose}>Close</button>
+          
         </div>
        
         
