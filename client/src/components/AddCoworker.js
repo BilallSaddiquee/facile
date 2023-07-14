90464a1565336d19d9c1af9b0237b2b1d4fd90ef
BILAL AHMAD AKBAR <95617094+Bilalkhan347@users.noreply.github.com>
2023-07-14 11:28:38 +0500

    Send Initation To Coworker
    node

--- client/src/components/AddCoworker.js
+++ client/src/components/AddCoworker.js
@@ -1,13 +1,12 @@
 import react, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import emailjs from "emailjs-com";
 import axios from "axios";
-function AddCoworker({ onClose }){
- 
+function AddCoworker({ onClose }) {
   const [email, setEmail] = useState("");
   const [err, seterr] = useState(false);
- const[errors,setErrors]=useState("");
-  
+  const [errors, setErrors] = useState("");
+  const navigate = useNavigate();
 
   function Addworker() {
     let regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
@@ -15,16 +14,49 @@ function AddCoworker({ onClose }){
       setErrors("Email is Required");
       seterr(true);
     } else if (!regex.test(email)) {
-      seterr(true)
+      seterr(true);
       setErrors("Invalid Email");
-    }else{
-
+    } else {
     }
-  
-  };
+  }
 
+  function sendEmail(e) {
+    e.preventDefault();
+    let regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
+    if (email === "") {
+      setErrors("Email is Required");
+      seterr(true);
+    } else if (!regex.test(email)) {
+      seterr(true);
+      setErrors("Invalid Email");
+    } else {
+      const templateParams = {
+        email: email,
+      };
 
-const EmailHandler = (e) => {
+      emailjs
+        .send(
+          "service_gnxkngh",
+          "template_6k27c9p",
+          templateParams,
+          "SshR7dvNO5QUpwqIQ"
+        )
+
+        .then(
+          (result) => {
+            console.log("Email sent successfully");
+            onClose();
+          },
+          (error) => {
+            console.log("Error sending email:", error.text);
+          }
+        );
+
+      setEmail("");
+    }
+  }
+
+  const EmailHandler = (e) => {
     let regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
 
     setEmail(e.target.value);
@@ -39,38 +71,20 @@ const EmailHandler = (e) => {
     }
   };
 
-
- 
-
- 
   return (
     <>
-
-
-
       <div className="container-fluid">
-        
-      <input
-            type="email"
-            placeholder="Email"
-            autoComplete="on"
-            onChange={EmailHandler}
-            value={email}
-          />
-          {err ? (
-            <span style={{ color: "rgb(247, 14, 14)" }}>
-              {errors}
-            </span>
-          ) : (
-            ""
-          )}
-          <button onClick={Addworker}>Send Invite</button>
-          <button onClick={onClose}>Close</button>
-         
-        </div>
-       
-        
-    
+        <input
+          type="email"
+          placeholder="Email"
+          autoComplete="on"
+          onChange={EmailHandler}
+          value={email}
+        />
+        {err ? <span style={{ color: "rgb(247, 14, 14)" }}>{errors}</span> : ""}
+        <button onClick={sendEmail}>Send Invite</button>
+        <button onClick={onClose}>Close</button>
+      </div>
     </>
   );
 }
