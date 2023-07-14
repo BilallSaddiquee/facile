15536f2fd3eed2033ed870efab73afde7f62ddcf
BILAL AHMAD AKBAR <95617094+Bilalkhan347@users.noreply.github.com>
2023-07-14 10:20:10 +0500

    Delete extra
    .

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import SignUp from "./components/SignUp";
 import Login from "./components/Login";
 import Landing from "./components/Landing";
 import Create_workspace from "./components/Create_workspace";
-import Test from "./components/Test";
 
 function App() {
   return (
@@ -13,9 +12,9 @@ function App() {
       <Routes>     
       <Route path="/signup" element={<SignUp />}></Route> 
       <Route path="/login" element={<Login />}></Route> 
-      <Route path="/land" element={<Landing />}></Route> 
+      <Route path="/" element={<Landing />}></Route> 
       <Route path="/work" element={<Create_workspace />}></Route> 
-      <Route path="/" element={<Test />}></Route> 
+
        
       </Routes>
      </BrowserRouter>
